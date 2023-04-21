8789a330a2046d04f3305b0439ab454f54d1f36b
malvinmuthuuri47 <malvinmuthuuri47@gmail.com>
2023-04-21 06:55:57 +0300

    Task 3

---
+++ setup_mysql_dev.sql
@@ -0,0 +1,9 @@
+-- Create a new database and user
+-- Give the user permissions and privileges
+-- If the user or Database exists, the script shouldn't fail
+
+CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+GRANT USAGE ON *.* TO 'hbnb_dev'@'localhost';
+GRANT ALL ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
+GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
